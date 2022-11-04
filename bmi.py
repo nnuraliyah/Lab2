fcac4aa3370ac7d44cdd201afe0f13a8d9e8f4f0
Aliyah <nuram.20@ichat.sp.edu.sg>
2022-11-04 15:18:18 +0800

    updating bmi

---
+++ bmi.py
@@ -0,0 +1,22 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight / (height * height)
+    print("BMI = " + str(bmi))
+
+    if (bmi < 18.5):
+        print("Underweight")
+    elif (bmi > 25.0):
+        print("Overweight")
+    else:
+        print("Normal Weight")
+
+calculate_bmi(weight=57, height=1.73)
+
+
+
+
+
+
+
